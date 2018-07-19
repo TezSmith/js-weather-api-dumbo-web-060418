e721d24c366fd39726e9560f132371bf4c81fda8
TezSmith <Montez.Smith1@gmail.com>
2018-07-19 16:48:34 -0400

    So fetch

--- js/init.js
+++ js/init.js
@@ -1,12 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var API_KEY = "Go get an API key";
+  var API_KEY = "cc74147b1369930d1c0f2594939a6b46";
   var CORS_WRAPPER = "https://cors-anywhere.herokuapp.com/"
-  var URL = CORS_WRAPPER + "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
-  var ctx = document.getElementById("NYCWeatherChart").getContext("2d")
+  var URL = CORS_WRAPPER + "https://api.darksky.net/forecast/cc74147b1369930d1c0f2594939a6b46/40.7127,-74.0059";
+  var ctx = document.getElementById("myChart").getContext("2d")
+  let weatherTimeTemp = {}
 
+  // makeRequest(URL, function(json) {
+  //   var data = generateDataSet(getHours(json), getFahrenheits(json));
+  //   var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
+  // });
 
-  makeRequest(URL, function(json) {
-    var data = generateDataSet(getHours(json), getFahrenheits(json));
-    var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
-  });
-});
+  fetch(URL, {
+    method: 'GET'
+  })
+  .then(function(resp){
+    return resp.json();
+  })
+  .then(function(resp){
+    let utcTimes = resp.hourly.data;
+     for (const hour in utcTimes) {
+       let utcSecond = utcTimes[hour].time;
+       weatherTimeTemp[utcSecond] = 0;
+       console.log(weatherTimeTemp)
+     }
+
+    })
+  })
+// let translatedDate = new Date(utcSeconds*1000);
+// let militaryHour = translatedDate.getHours();
+// console.log(militaryHour)
--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,15 @@
-function getFahrenheits(result){
-  // Your code goes here
-}
-
-function getHours(result){
-  // Your code goes here
-}
-
-function generateDataSet(labels, data) {
-  // Your code goes here
-}
-
-function makeRequest(endpoint, success) {
-  // Your code goes here
-}
+// function getFahrenheits(result){
+//   // Your code goes here
+// }
+//
+// function getHours(result){
+//   // Your code goes here
+// }
+//
+// function generateDataSet(labels, data) {
+//   // Your code goes here
+// // }
+//
+// function makeRequest(endpoint, success) {
+//   // Your code goes here
+// }
